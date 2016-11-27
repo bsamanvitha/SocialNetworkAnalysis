3e1931771124d485a5ad3e3083188571229acbe8
Samanvitha Basole <s97basole@gmail.com>
2016-11-27 13:07:02 -0800

    Plot dendrogram objects

--- KarateClub/KarateClub.py
+++ KarateClub/KarateClub.py
@@ -3,9 +3,8 @@ import igraph
 
 karateGraph = igraph.read('karate.GraphML', format = 'graphml')
 plot(karateGraph, "1original.png")
-plot(karateGraph.community_edge_betweenness(), "2girvanNewman.png") #returns a dendrogram object
+plot(karateGraph.community_edge_betweenness().as_clustering(), "2girvanNewman.png")
 plot(karateGraph.community_multilevel(), "3louvainMethod.png")
-plot(karateGraph.community_fastgreedy(), "4fastGreedy.png") #returns a dendrogram object
-plot(karateGraph.community_walktrap(), "5randomWalks.png") # returns a dondrogram object
+plot(karateGraph.community_fastgreedy().as_clustering(), "4fastGreedy.png")
+plot(karateGraph.community_walktrap().as_clustering(), "5randomWalks.png")
 plot(karateGraph.community_label_propagation(), "6labelPropagation.png")
-
