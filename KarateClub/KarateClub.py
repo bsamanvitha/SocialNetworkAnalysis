baf22fe4414b9bd33e854fba57a6da15984849d9
Samanvitha Basole <s97basole@gmail.com>
2016-11-27 14:57:51 -0800

    Change vertex size to 10

--- KarateClub/KarateClub.py
+++ KarateClub/KarateClub.py
@@ -2,9 +2,13 @@ from igraph import *
 import igraph
 
 karateGraph = igraph.read('karate.GraphML', format = 'graphml')
-plot(karateGraph, "1original.png")
-plot(karateGraph.community_edge_betweenness().as_clustering(), "2girvanNewman.png")
-plot(karateGraph.community_multilevel(), "3louvainMethod.png")
-plot(karateGraph.community_fastgreedy().as_clustering(), "4fastGreedy.png")
-plot(karateGraph.community_walktrap().as_clustering(), "5randomWalks.png")
-plot(karateGraph.community_label_propagation(), "6labelPropagation.png")
+
+visual_style = {}
+visual_style["vertex_size"] = 10
+
+plot(karateGraph, "1original.png", **visual_style)
+plot(karateGraph.community_edge_betweenness().as_clustering(), "2girvanNewman.png", **visual_style)
+plot(karateGraph.community_multilevel(), "3louvainMethod.png", **visual_style)
+plot(karateGraph.community_fastgreedy().as_clustering(), "4fastGreedy.png", **visual_style)
+plot(karateGraph.community_walktrap().as_clustering(), "5randomWalks.png", **visual_style)
+plot(karateGraph.community_label_propagation(), "6labelPropagation.png", **visual_style)
