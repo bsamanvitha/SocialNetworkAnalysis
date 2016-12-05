f39bfa24e558b5aea79553d9eae1d347e7cf5772
Ketki Kulkarni <ketki.kulkarni@sjsu.edu>
2016-12-04 19:05:06 -0800

    Latest changes
    Includes modularity and no of clusters

--- BarabasiGraph/communityDetectionOnBarabasiGraph.py
+++ BarabasiGraph/communityDetectionOnBarabasiGraph.py
@@ -1,23 +1,51 @@
 import igraph
 
 
-graph = igraph.Graph.Barabasi(100, 2)
+graph = igraph.Graph.Barabasi(1000, 1)
+#graph.vs["label"] = range(1000)
+
+visual_style = {}
+visual_style["edge_curved"] = False
+visual_style["vertex_color"] = "#95D2CB"
+visual_style["vertex_size"] = 10
+visual_style["vertex_label_size"] = 5
 
 #igraph.summary(graph)
-igraph.plot(graph,"barabasi.png")
+igraph.plot(graph,"barabasiGraph.png", **visual_style)
+
+visual_style1 = {}
+visual_style1["edge_curved"] = False
+visual_style1["vertex_size"] = 10
+visual_style1["vertex_label_size"] = 5
+
 
 #print(communities)
 louvainCommunity = graph.community_multilevel()
-igraph.plot(louvainCommunity, "LouvainOnBarabasi.png")
+#print louvainCommunity
+print "No of community = {}".format(louvainCommunity.cluster_graph().vcount())
+print "Modularity of communities detected by Louvain Algorithm on the Albert-Barabasi model =  {}".format(louvainCommunity.modularity)
+igraph.plot(louvainCommunity, "LouvainOnBarabasi.png", **visual_style1)
 
-girvanNewmanCommunity = graph.community_edge_betweenness()
-igraph.plot(girvanNewmanCommunity.as_clustering(), "GirvanNewmanOnBarabasi.png")
+girvanNewmanCommunity = graph.community_edge_betweenness().as_clustering()
+#print girvanNewmanCommunity
+print "No of community = {}".format(girvanNewmanCommunity.cluster_graph().vcount())
+print "Modularity of communities detected by Girvan-Newman Algorithm on the Albert-Barabasi model  =  {}".format(girvanNewmanCommunity.modularity)
+igraph.plot(girvanNewmanCommunity, "GirvanNewmanOnBarabasi.png", **visual_style1)
 
 labelPropagationCommunity = graph.community_label_propagation()
-igraph.plot(labelPropagationCommunity, "LabelPropagationOnBarabasi.png")
+#print labelPropagationCommunity
+print "No of community = {}".format(labelPropagationCommunity.cluster_graph().vcount())
+print "Modularity of communities detected by Label Propagation Algorithm on the Albert-Barabasi model =  {}".format(labelPropagationCommunity.modularity)
+igraph.plot(labelPropagationCommunity, "LabelPropagationOnBarabasi.png", **visual_style1)
 
-fastGreedyCommunity = graph.community_fastgreedy()
-igraph.plot(fastGreedyCommunity.as_clustering(), "FastGreedyOnBarabasi.png")
+fastGreedyCommunity = graph.community_fastgreedy().as_clustering()
+#print fastGreedyCommunity
+print "No of community = {}".format(fastGreedyCommunity.cluster_graph().vcount())
+print "Modularity of communities detected by Fast-greedy Algorithm on the Albert-Barabasi model =  {}".format(fastGreedyCommunity.modularity)
+igraph.plot(fastGreedyCommunity, "FastGreedyOnBarabasi.png", **visual_style1)
 
-randomWalkCommunity = graph.community_walktrap()
-igraph.plot(randomWalkCommunity.as_clustering(), "RandomWalkOnBarabasi.png")
+randomWalkCommunity = graph.community_walktrap().as_clustering()
+#print randomWalkCommunity
+print "No of community = {}".format(randomWalkCommunity.cluster_graph().vcount())
+print "Modularity of communities detected by Random walk Algorithm on the Albert-Barabasi model =  {}".format(randomWalkCommunity.modularity)
+igraph.plot(randomWalkCommunity, "RandomWalkOnBarabasi.png", **visual_style1)
